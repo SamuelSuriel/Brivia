9a7ba0da1a892876f58f570d8f50a1ca71fcb569
Samuel Ramirez <34385530+SamuelSuriel@users.noreply.github.com>
2020-05-18 16:43:35 -0400

    Restore last arquitecture

---
+++ WebAPI/Migrations/20200518200831_InitialCreate.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IdUser1 = table.Column<int>(nullable: false),
+                    IdUser2 = table.Column<int>(nullable: false),
+                    Turn = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Round = table.Column<int>(nullable: false),
+                    CorrectAnwer1 = table.Column<int>(nullable: false),
+                    Person1 = table.Column<int>(nullable: false),
+                    CorrectAnwer2 = table.Column<int>(nullable: false),
+                    Person2 = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    Lastname = table.Column<string>(nullable: false),
+                    Username = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    GamesWon = table.Column<int>(nullable: false),
+                    MissedGames = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GameDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IdGameId = table.Column<int>(nullable: true),
+                    IdUserId = table.Column<int>(nullable: true),
+                    IdCategoryId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GameDetails_Categories_IdCategoryId",
+                        column: x => x.IdCategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_GameDetails_Games_IdGameId",
+                        column: x => x.IdGameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_GameDetails_Users_IdUserId",
+                        column: x => x.IdUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: false),
+                    IdCategoryId = table.Column<int>(nullable: true),
+                    IncorrectAnswer1 = table.Column<string>(nullable: false),
+                    IncorrectAnswer2 = table.Column<string>(nullable: false),
+                    IncorrectAnswer3 = table.Column<string>(nullable: false),
+                    IncorrectAnswer4 = table.Column<string>(nullable: false),
+                    CorrectAnswer = table.Column<int>(nullable: false),
+                    IdUserId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Questions_Categories_IdCategoryId",
+                        column: x => x.IdCategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Questions_Users_IdUserId",
+                        column: x => x.IdUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Records",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IdUserId = table.Column<int>(nullable: true),
+                    IdCategoryId = table.Column<int>(nullable: true),
+                    CorrectAnswer = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Records", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Records_Categories_IdCategoryId",
+                        column: x => x.IdCategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Records_Users_IdUserId",
+                        column: x => x.IdUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameDetails_IdCategoryId",
+                table: "GameDetails",
+                column: "IdCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameDetails_IdGameId",
+                table: "GameDetails",
+                column: "IdGameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameDetails_IdUserId",
+                table: "GameDetails",
+                column: "IdUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_IdCategoryId",
+                table: "Questions",
+                column: "IdCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_IdUserId",
+                table: "Questions",
+                column: "IdUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Records_IdCategoryId",
+                table: "Records",
+                column: "IdCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Records_IdUserId",
+                table: "Records",
+                column: "IdUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GameDetails");
+
+            migrationBuilder.DropTable(
+                name: "Questions");
+
+            migrationBuilder.DropTable(
+                name: "Records");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ WebAPI/Models/Model.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Brivia : DbContext
+    {
+        public Brivia(DbContextOptions<Brivia> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Record> Records { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<GameDetail> GameDetails { get; set; }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Lastname { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public int GamesWon { get; set; }
+        public int MissedGames { get; set; }
+    }
+
+    public class Question
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Category IdCategory { get; set; }
+        [Required]
+        public string IncorrectAnswer1 { get; set; }
+        [Required]
+        public string IncorrectAnswer2 { get; set; }
+        [Required]
+        public string IncorrectAnswer3 { get; set; }
+        [Required]
+        public string IncorrectAnswer4 { get; set; }
+        [Required]
+        public int CorrectAnswer { get; set; }
+        public User IdUser { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+    }
+
+    public class Record
+    {
+        public int Id { get; set; }
+        public User IdUser { get; set; }
+        public Category IdCategory { get; set; }
+        public int CorrectAnswer { get; set; }
+    }
+
+    public class Game
+    {
+        public int Id { get; set; }
+        public int IdUser1 { get; set; }
+        public int IdUser2 { get; set; }
+        public int Turn { get; set; }
+        public DateTime Date { get; set; }
+        public int Round { get; set; }
+        public int CorrectAnwer1 { get; set; }
+        public int Person1 { get; set; }
+        public int CorrectAnwer2 { get; set; }
+        public int Person2 { get; set; }
+    }
+
+    public class GameDetail
+    {
+        public int Id { get; set; }
+        public Game IdGame { get; set; }
+        public User IdUser { get; set; }
+        public Category IdCategory { get; set; }
+    }
+}
